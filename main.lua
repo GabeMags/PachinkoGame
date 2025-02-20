0094add7f055b1ea39b642a20699bce545880c3e
GabeMags <magallanes.gabe@gmail.com>
2025-02-20 00:15:43 -0800

    Begin work on game. Rid some old code from tutorials. Added a ball and some gravity.

--- main.lua
+++ main.lua
@@ -1,10 +1,28 @@
 -- Allows for live console output
 io.stdout:setvbuf('no')
 
-
--- Begin code
-function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+function love.load()
+    ball = {
+        x = 200,
+        y = 100,
+        radius = 20,
+        speedY = 0, -- Vertical speed
+        gravity = 500 -- Pixels per second squared
+    }
 end
 
---Redundant comment for storing credentials locally (smh)
+function love.update(dt)
+     -- Apply gravity
+    ball.speedY = ball.speedY + ball.gravity * dt
+    ball.y = ball.y + ball.speedY * dt
+
+    -- Stop the ball at the bottom of the screen (basic collision)
+    if ball.y + ball.radius > love.graphics.getHeight() then
+        ball.y = love.graphics.getHeight() - ball.radius
+        ball.speedY = 0
+    end
+end
+
+function love.draw()
+    love.graphics.circle("fill", ball.x, ball.y, ball.radius)
+end
